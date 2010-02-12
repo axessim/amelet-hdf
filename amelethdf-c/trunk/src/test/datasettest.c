fb4a2fa3d1b741f34dd52c8ac5d4e531b16db417
cyril.giraudon <cyril.giraudon@24d24c72-a858-11de-8ba5-91705c1537ed>
2010-02-12 10:44:02 +0000

    Test : Spaces replace Tabs

--- amelethdf-c/trunk/src/test/datasettest.c
+++ amelethdf-c/trunk/src/test/datasettest.c
@@ -12,34 +12,33 @@
 #include <complex.h>
 #include <stdio.h>
 
-int main(argc,argv)
-int argc;
-char *argv[];
+int main(argc, argv)
+    int argc;char *argv[];
 {
-	char * filename;
-	dataset_t ds;
+    char * filename;
+    dataset_t ds;
 
-	hid_t file_id;
+    hid_t file_id;
     // init
-	H5open();
-	if(argc==1)
-	{
-	  printf("missing input file name\n");
-	  return -1;
-	}
-	filename = (char *)malloc(100*sizeof(char));
-	strcpy(filename,argv[1]);
-	// Read for the hdf5 file
-	printf("Reading %s\n",filename);
-	file_id = H5Fopen(filename,H5F_ACC_RDWR,H5P_DEFAULT);
+    H5open();
+    if (argc == 1)
+    {
+        printf("missing input file name\n");
+        return -1;
+    }
+    filename = (char *) malloc(100 * sizeof(char));
+    strcpy(filename, argv[1]);
+    // Read for the hdf5 file
+    printf("Reading %s\n", filename);
+    file_id = H5Fopen(filename, H5F_ACC_RDWR, H5P_DEFAULT);
 
-	printf("Reading dataset ...\n");
-	//Integer dataset
-	printf("/int_dataset\n");
-	ds = read_dataset(file_id,"/int_dataset");
-	printf(" %s",dataset_to_string(ds));
+    printf("Reading dataset ...\n");
+    //Integer dataset
+    printf("/int_dataset\n");
+    ds = read_dataset(file_id, "/int_dataset");
+    printf(" %s", dataset_to_string(ds));
 
-	H5Fclose(file_id);
-	free(filename);
-	return 0;
+    H5Fclose(file_id);
+    free(filename);
+    return 0;
 }
--- amelethdf-c/trunk/src/test/stringdatasettest.c
+++ amelethdf-c/trunk/src/test/stringdatasettest.c
@@ -1,46 +1,44 @@
 /*
- * datasettest.c
+ *  stringdatasettest.c
  *
  *  Created on: 2010/02/04
  *  Author: Cyril Giraudon
  */
 
-
 #include <hdf5.h>
 #include <dataset.h>
 
-int main(argc,argv)
-int argc;
-char *argv[];
+int main(argc, argv)
+    int argc;char *argv[];
 {
     char * filename;
     int values_len = 34;
 
     char values[2][3][34] =
-    {{"/floatingType/far_field_theta_phi",
-      "farfield.h5",
-      "/floatingType/far_field_theta_phi"},
+    {
+    { "/floatingType/far_field_theta_phi", "farfield.h5",
+            "/floatingType/far_field_theta_phi" },
 
-     {"/floatingType/Enear_field_plane1",
-      "monitor_plane1_efield.h5",
-      "/floatingType/Enear_field_plane1"}};
+    { "/floatingType/Enear_field_plane1", "monitor_plane1_efield.h5",
+            "/floatingType/Enear_field_plane1" } };
     int rank = 2;
-    hsize_t dims[2] = {2,3};
+    hsize_t dims[2] =
+    { 2, 3 };
 
     hid_t file_id;
 
     // Initialization
     H5open();
-    if(argc==1)
+    if (argc == 1)
     {
         printf("missing input file name\n");
         return -1;
     }
-    filename = (char *)malloc(100*sizeof(char));
-    strcpy(filename,argv[1]);
+    filename = (char *) malloc(100 * sizeof(char));
+    strcpy(filename, argv[1]);
 
     // Read for the hdf5 file
-    printf("Creating %s ...\n",filename);
+    printf("Creating %s ...\n", filename);
     file_id = H5Fcreate(filename, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
     write_string_dataset(file_id, "string_data", values_len, rank, dims, values);
 
