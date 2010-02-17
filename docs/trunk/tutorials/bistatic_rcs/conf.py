56477fceb908a10ab1dde234f62a92b952f13bb4
cyril.giraudon <cyril.giraudon@24d24c72-a858-11de-8ba5-91705c1537ed>
2010-02-17 11:39:10 +0000

    Ready for release 0.1

--- docs/trunk/tutorials/bistatic_rcs/conf.py
+++ docs/trunk/tutorials/bistatic_rcs/conf.py
@@ -172,7 +172,7 @@ htmlhelp_basename = 'Bistaticrcsexampledoc'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'Bistaticrcsexample.tex', u'Bistatic rcs example Documentation',
+  ('index', 'bistaticrcsexample.tex', u'Bistatic rcs example Documentation',
    u'Cyril Giraudon', 'manual'),
 ]
 
