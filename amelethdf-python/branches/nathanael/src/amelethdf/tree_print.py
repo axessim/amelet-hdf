866de83deb0e3bd83902d4d368b809bc38b0b1ec
nathanael.muot <nathanael.muot@24d24c72-a858-11de-8ba5-91705c1537ed>
2010-02-25 16:21:08 +0000

    add pretty tree print

--- amelethdf-python/branches/nathanael/src/amelethdf/tree_print.py
+++ amelethdf-python/branches/nathanael/src/amelethdf/tree_print.py
@@ -23,6 +23,21 @@ TTL_N = '  '
 TL_TAG = ' '
 
 
+GROUP_TAG = '/'
+S_TAB = '['
+E_TAB = ']'
+S_DATA = '{'
+E_DATA = '}'
+
+
+ATTR1 = '@'
+ATTR2 = '#'
+ATTR_SEP = ', '
+ATTR_ASIGN = '='
+S_ATTRS = '('
+E_ATTRS = ')'
+
+
 class TreeLink:
     def __init__(self):
         self.tree_link = []
@@ -51,3 +66,90 @@ class TreeLink:
             
     def __str__(self):
         return ''.join(self.tree_link)
+    
+    
+    
+    
+class DrawingTree(object):
+    """
+    
+    Friend of Tree 
+    """
+    
+    def __get_children_type(self):
+        return TTL_G
+    
+    def __get_attrs_name(self):
+        return self.attrs_name
+    
+    def __get_attr_value(self, name):
+        return None
+    
+    def __get_attr_type(self, name):
+        return ATTR1
+    
+    def tostring(self):
+        """Return a tree string of this model node.
+        """
+        return self.__tostring()
+    
+    #---------------------------------------------------------------------------
+    # Methods to build string tree view
+    #---------------------------------------------------------------------------
+    def __tostring(self, tree_link=TreeLink()):
+        """Internal method to build tree string of this model node.
+        """
+        s = self.__tostring_line(tree_link=tree_link)
+        
+        bt = self.__get_children_type()
+        
+        
+        tree_link.append(bt)
+        items = self.iter_children()
+        i = 0
+        for item in items:
+            i += 1
+            tree_link.follow(bt, len(items) == i)
+            s += '\n' + item.__tostring(tree_link)
+        tree_link.down()
+                
+        return s
+    
+    def __tostring_line(self, tree_link=None):
+        """Internal method that return model node string. 
+        """
+        s = ""
+        
+        if tree_link:    
+            s += str(tree_link) + self.name
+        else:
+            s += self.name
+            
+        s += self.__tostring_attrs() + self.__tostring_type()
+        
+        return s 
+            
+    def __tostring_attrs(self):
+        """Internal method that build string view of model attributes.
+        """
+        
+        attrs = []
+        for attr_name in self.__get_attrs_name():
+            val = self.__get_attr_value(attr_name)
+            attr = "%s%s" % (self.__get_attr_type(attr_name), attr_name)
+            
+            if val != None:
+                attr = "%s%s%s" % (attr, ATTR_ASIGN, val)
+            attrs.append(attr)
+                
+        if attrs:    
+            return "%s%s%s" % (S_ATTRS, ATTR_SEP.join(attrs), E_ATTRS)
+        else:
+            return ""
+    
+    
+    def __tostring_type(self):
+        """Internal method to build string view model type
+        """
+        return GROUP_TAG
+    
