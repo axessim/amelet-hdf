618bbc466d0a68e02fef99168eeac00aa2e35b33
nathanael.muot <nathanael.muot@24d24c72-a858-11de-8ba5-91705c1537ed>
2013-02-19 11:27:44 +0000

    fix symbols visibility for gcc

--- amelethdf-c/branches/vlse/src/amelethdf/ah5_config.h
+++ amelethdf-c/branches/vlse/src/amelethdf/ah5_config.h
@@ -6,10 +6,30 @@
 #ifndef AH5_CONFIG_H_
 #define AH5_CONFIG_H_
 
-#ifdef AMELETHDF_C_LIBRARY
-# define AH5_EXPORT __declspec(dllexport)
+// See http://gcc.gnu.org/wiki/Visibility
+#if defined _WIN32 || defined __CYGWIN__
+# ifdef AMELETHDF_C_LIBRARY
+#  ifdef __GNUC__
+#   define AH5_EXPORT __attribute__ ((dllexport))
+#  else
+#   define AH5_EXPORT __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
+#  endif
+# else
+#  ifdef __GNUC__
+#   define AH5_EXPORT __attribute__ ((dllimport))
+#  else
+#   define AH5_EXPORT __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
+#  endif
+# endif
+#define AH5_LOCAL
 #else
-# define AH5_EXPORT __declspec(dllimport)
+# if __GNUC__ >= 4
+#  define AH5_EXPORT __attribute__ ((visibility ("default")))
+#  define AH5_LOCAL  __attribute__ ((visibility ("hidden")))
+# else
+#  define AH5_PUBLIC
+#  define AH5_LOCAL
+# endif
 #endif
 
 #endif  // AH5_CONFIG_H_
